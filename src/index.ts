20782caec9687ace6ac1ee009facbd5b5b815ef9
Billy Chubb <billy@bchubb.co.uk>
2023-05-11 21:32:43 +0100

    honstly dont remember where im at

--- src/index.ts
+++ src/index.ts
@@ -3,55 +3,36 @@ import promptSync from 'prompt-sync';
 import clear from 'clear';
 import fs from 'fs';
 import scrabbleLetters from './assets/letters.json' assert {type: "json"};
-
-
 const scrabbleWords:string[] = fs.readFileSync('./dist/assets/words.txt', 'utf-8').split('\r\n');
-
-type Letters = {
-    [key: string] : {
-        ["points"]: number | null,
-        ["amount"]: number
-    }
-};
-
 
 const prompt: promptSync.Prompt = promptSync();
 
 
-function setMyLetters(input: string[]): string[] {
-    input.map((letter) => {
-        myLetters.hasOwnProperty(letter) ? myLetters[letter].amount ++ : myLetters[letter] = { amount: 1, points: scrabbleLetters[letter].points };
-        /*if (myLetters.hasOwnProperty(letter)){ myLetters[letter].amount ++; } else { myLetters[letter] = {amount: 1, points: 1}; }*/
-    });
-    return myLetters;
-}
 
-function getLetters(): void {
+function getLetters(): string[] {
     let input:string[] = [];
     while (input.length !== 9){
         clear(); 
         input = prompt('enter your 9 characters').toUpperCase().split('');
     }
-    //myLetters = setMyLetters(input)
-    myLetters = input;
+    return input;
 }
 
-
-
-
-function hasLetters(word: string): boolean{
+function hasLetters(word: string, myLetters: string[]): boolean{
     let res: boolean = true;
+
+    let temp: string[] = Object.assign([], myLetters);
+
     word.split('').map((wordLetter) => {
-        if (!myLetters.includes(wordLetter)){
+        if (!temp.includes(wordLetter)){
             res = false;
         }
         else {
+            temp.splice(temp.indexOf(wordLetter), 1);
         }
     })
     return res;
-
 }
-
 
 function getPoints(word:string): number {
     let total: number = 0;
@@ -61,17 +42,32 @@ function getPoints(word:string): number {
     return total;
 }
 
+
+
+
+
+
+
+
+
+
+
+
 let myLetters: string[] = [];
 let biggest: string = '';
 
 
-getLetters();
+myLetters = getLetters();
 
 const possible: string[] = scrabbleWords.filter((word) => {
-    return hasLetters(word);
+    return hasLetters(word, myLetters);
 });
 possible.map((word) => {
     biggest = getPoints(word) > getPoints(biggest) ? word : biggest;
+})
+
+possible.filter((word) => {
+    return getPoints(word) > getPoints(biggest) ? word : biggest;
 })
 
 console.log(`Best Word: ${biggest}\nWith score: ${getPoints(biggest)}\n With no multiplier`);
