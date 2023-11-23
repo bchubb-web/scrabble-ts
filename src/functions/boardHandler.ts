04f5533b5a2ea1c98e4e585960a790f3f858fd68
Billy Chubb <billy@bchubb.co.uk>
2023-11-23 10:33:23 +0000

    small changes

--- src/functions/boardHandler.ts
+++ src/functions/boardHandler.ts
@@ -35,6 +35,12 @@ export function getMultiplier(x: number, y: number, size: number): Multiplier {
 }
 
 
+/**
+*
+*
+*
+*
+*/
 export function outputBoard(board: Board, size: number): void { let output = '';
     board.forEach((tile, index) => {
         output = output + (index % size === 0 ? '\n' + tile.multiplier.power : tile.multiplier.power);
@@ -42,9 +48,28 @@ export function outputBoard(board: Board, size: number): void { let output = '';
     console.log(output);
 }
 
+/**
+* takes simulated coordinates and returns the 1D index for the board
+*
+* @param X x co-ordinate
+*
+* @param Y y co-ordinate
+*
+* @returns Index for position on the board
+*/
 export function getIndex(x:number, y:number, length:number): number {
     return y*length + x;
 }
+
+/**
+* takes the INDEX and returns the simulated coordinates
+*
+* @param index the index from the 1d array
+*
+* @param size the size of the array
+*
+* @returns tuple [row, column]
+*/
 export function getCoOrds(index: number, size: number): [number, number] {
     return [Math.floor(index/size), index%size];
 }
--- src/functions/placeHandler.ts
+++ src/functions/placeHandler.ts
@@ -1,12 +1,25 @@
-import { Dir } from "fs";
+import { getIndex, getCoOrds } from "./boardHandler.js";
 
-export function placeWord(word: string, startTile: number, direction: Direction, board: Board): void {
+export function placeWord(word: string, startTile: number, direction: Direction, board: Board, size: number): boolean {
+
+    // check all places are on the board / on the same plane
+    if (!validatePlane(startTile, direction, word, size)) return false;
+    //  check all those places are empty or contain the same letter
+    if (!validateLetters(word, startTile, )) return false;
+    //  place word
+
+    
+
     let invalidLetters = word.split('').map((letter, index) => {
-             isValidLetter(letter, startTile, board)
+        isValidLetter(letter, startTile, board)
     })
     if (invalidLetters.length > 0){
-        return;
+        return false;
     }
+    word.split('').map((letter, index) => {
+        
+    })
+    
 }
 function placeLetter(letter: string, tile: number, board: Board): boolean {
     if (board[tile].letter !== undefined || board[tile].letter.char === letter) {
@@ -14,13 +27,41 @@ function placeLetter(letter: string, tile: number, board: Board): boolean {
     }
 }
 
-function isValidLetter(letter: string, tile: number, board: Board):boolean {
-    return board[tile].letter.char !== letter;
+function isValidLetter(letter: string, index: number, board: Board):boolean {
+    return board[index].letter.char !== letter;
 }
 
+/**
+*
+* @param - 
+*/
 function getRelativeIndex(start: number, offset: number, direction: Direction, size: number): number {
     if (direction === 'Horizontal') {
-        return start + offset
+        return start + offset;
     }
     return start + (size * offset);
+}
+
+
+function validateLetters(word){
+    word.forEach((letter: string, index: number) => {
+        
+    });
+}
+
+
+
+
+function getPlaneIndex(direction: Direction, coOrds: [number, number]): number {
+    return direction == 'Horizontal' ? coOrds[0] : coOrds[1];
+}
+
+function validatePlane(startTile: number, direction: Direction, word: string, size: number): boolean {
+    let initalPlane: number = getPlaneIndex(direction, getCoOrds(startTile,size));
+    for (let i=1; i < word.length; i++) {
+        if (getPlaneIndex(direction,getCoOrds(startTile+i,size)) !== initalPlane) {
+            return false;
+        }
+    }
+    return true;
 }
